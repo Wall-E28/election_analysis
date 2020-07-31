1861e46ad9168d41cc41a6fb437b29a077cb8c3c
Wall-E28 <rileybrowne@gmail.com>
2020-07-31 12:15:49 -0700

    "Updating pypoll.py to include voting percentages per candidate"

--- py_poll.py
+++ py_poll.py
@@ -11,6 +11,7 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 #Create variables for voting 
 total_votes = 0
 candidate_options = []
+candidate_votes = {}
 
 # Open Election Results
 with open(file_to_load, 'r') as election_data:
@@ -28,13 +29,32 @@ with open(file_to_load, 'r') as election_data:
         # Print candidate name from each row
         candidate_name = row[2]
         
-        # Add unique candidate name to candidate list
+       
         if candidate_name not in candidate_options:
+            
+            # Add unique candidate name to candidate list
             candidate_options.append(candidate_name)
+            
+            # Track votes for each candidate
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    
+    # Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    
+    # Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    
+    # Print the candidate name and percentage of votes.
+    print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
 
 #Print election variables
 print(total_votes)
-print(candidate_options)
+print(candidate_votes)
 
 # # Open file
 # with open(file_to_save, 'w') as election_analysis_file:
