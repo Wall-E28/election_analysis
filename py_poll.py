0c8fe0a3f7f7795926e4bdd98649a5b33b3738fc
Wall-E28 <rileybrowne@gmail.com>
2020-07-31 11:16:00 -0700

    "Adding reading functionality to election_results.csv"

--- py_poll.py
+++ py_poll.py
@@ -1,17 +1,26 @@
+# Add dependencies
 import csv
 import os
 
-# # Create path to Election Results 
-# file = os.path.join("Resources", "election_results.csv")
-
-# # Open Election Results
-# with open(file) as election_data:
-
-#     # Read Election Results 
-#     print(election_data)
+# Create path to Election Results 
+file_to_load = os.path.join("Resources", "election_results.csv")
 
 # Create a filename variable to a direct path to a file
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open Election Results
+with open(file_to_load, 'r') as election_data:
+
+    # Read Election Results 
+    file_reader = csv.reader(election_data)
+
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+    # # Print each row in the CSV file.
+    # for row in file_reader:
+    #     print(row)
 
 # Open file
 with open(file_to_save, 'w') as election_analysis_file:
